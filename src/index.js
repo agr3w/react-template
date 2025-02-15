78fd471fa1e8d58ee21c1393c00642d5cd5c777a
Weslley Luiz Kampa <91283681+agr3w@users.noreply.github.com>
2025-02-15 15:32:16 -0300

    Update index.js
    Arrumando erro de escrita

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import "./styles/reset.css";
+import "./styles/Reset.css";
 import AppRouter from './Router';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
